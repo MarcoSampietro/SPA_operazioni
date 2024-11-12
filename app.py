3954b0f941015e13a4fc6e3d77b1db93445a4617
Marco Sampietro <marco.sampietro1574@gmail.com>
2024-11-12 11:46:13 +0000

    calcolatrice finita

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,10 +7,23 @@ def homepage():
 
 @app.route('/calcola', methods=['POST'])
 def calcola():
+    #Istruzioni per prendere le informazioni in arrivo dal front end
     dati = request.get_json()
-    num1 = data.get('num1', 0)
-    num2 = data.get('num2', 0)
-    return render_template('index.html')
+    if dati:
+        num1 = dati.get('num1')
+        num2 = dati.get('num2')
+        operazione = dati.get('operazione')
+        #elaborazione dati
+        if operazione == 'addizione':
+            risultato = num1 + num2
+        elif operazione == 'sottrazione':
+            risultato = num1 - num2
+        elif operazione == 'moltiplicazione':
+            risultato = num1 * num2
+        elif operazione == 'divisione':
+            risultato = num1 // num2
+            #restituire i dati al front end
+        return jsonify(risultato = risultato)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
